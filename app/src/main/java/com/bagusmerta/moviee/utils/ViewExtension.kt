edb58a3c172f85f539d02628f61d1692cc9b910b
Bagus Merta <merta.bagus24@gmail.com>
2022-03-05 13:13:09 +0800

    [bagus] add error handling on imageview extension

--- app/src/main/java/com/bagusmerta/moviee/utils/ViewExtension.kt
+++ app/src/main/java/com/bagusmerta/moviee/utils/ViewExtension.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import com.bagusmerta.core.utils.Constants.IMAGE_URL
+import com.bagusmerta.moviee.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 
@@ -12,6 +13,7 @@ fun ImageView.loadImage(url: String?){
     Glide.with(context)
         .load("${IMAGE_URL}${url}")
         .diskCacheStrategy(DiskCacheStrategy.ALL)
+        .error(R.drawable.ic_baseline_broken_image_24)
         .into(this)
 }
 
