92827bcc824379ebfb2f89a5a791320d452a4cf5
Bagus Merta <merta.bagus24@gmail.com>
2023-06-15 23:02:31 +0800

    chore: implement spotless for kt and kts files

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,3 +1,17 @@
+/*
+ * Designed and developed by 2023 gusentanan (Bagus Merta)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -19,6 +33,7 @@ tasks.withType<KotlinCompile>().configureEach {
 dependencies {
     compileOnly(libs.android.gradlePlugin)
     compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.spotless.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
 }
 
@@ -39,6 +54,10 @@ gradlePlugin {
         register("androidLibrary") {
             id = "moviee.android.library"
             implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidSpotless") {
+            id = "moviee.android.spotless"
+            implementationClass = "SpotlessConventionPlugin"
         }
     }
 }
